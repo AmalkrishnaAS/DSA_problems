94d2fc4e8d7125e27d30824844f40f4ec3ac4ccd
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-06-13 12:11:40 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (25.49%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& v) {
+        
+        
+    int n=v.size();
+    vector <int> dp(n+1);
+    dp[0]=0;
+    dp[1]=v[0];
+    for(int i=1;i<n;i++)
+    {
+        dp[i+1]=max(dp[i],v[i]+dp[i-1]);
+    }
+        return dp[n];
+    }
+};
