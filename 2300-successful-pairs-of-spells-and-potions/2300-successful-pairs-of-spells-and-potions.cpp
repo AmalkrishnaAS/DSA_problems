22e94e5ba6cfa9ddc7f738941b24d7732a2c661b
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-06-13 17:06:37 +0530

    Time: 342 ms (50.00%), Space: 99.9 MB (50.00%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        
+        sort(potions.begin(),potions.end());
+        
+        int n=potions.size();
+       
+        vector<int> ans;
+        
+        
+        for (int s:spells)
+        {
+            int l=0,r=n-1;
+              int res=n;
+            
+            while(l<=r)
+            {
+                int mid=(r+l)/2;
+               
+                if(1LL*s*potions[mid]>=success) //1LL converts to long long
+                {
+                    res=mid;
+                    r=mid-1;
+                }
+                else
+                {
+                    l=mid+1;
+                }
+                    
+            }
+            
+            ans.push_back(n-res);
+        }
+        
+        return ans;
+        
+    }
+};
