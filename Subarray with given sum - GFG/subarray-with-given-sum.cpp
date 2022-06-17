01f45fbf3e77104abb383899f4671c5056da4150
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-06-17 18:13:59 +0530

    Added solution - LeetHub

--- Subarray with given sum - GFG/subarray-with-given-sum.cpp
+++ Subarray with given sum - GFG/subarray-with-given-sum.cpp
@@ -7,55 +7,40 @@ using namespace std;
 class Solution
 {
     public:
-   
-    vector<int> subarraySum(int arr[], int n, long long s)
+    //Function to find a continuous sub-array which adds up to a given number.
+    vector< int> subarraySum(int arr[], int n, long long s)
     {
-        int last=0;
-        int start=0;
-        unsigned long long  currsum=0;
-        bool flag=false;
-        vector<int>res;
+        // Your code here
         
-        //iterating over the array.
-        for(int i=0;i<n;i++)
-        {
-            //storing sum upto current element.
-            currsum += arr[i];
-            
-            //checking if current sum is greater than or equal to given number.
-            if(currsum>=s)
-            {
-                last=i;
-                //we start from starting index till current index and do the 
-                //excluding part while s(given number) < currsum.
-                while(s<currsum && start<last)
-                {
-                    //subtracting the element from left i.e., arr[start]
-                    currsum -= arr[start];
-                    ++start;
-                }
-                
-                //now if current sum becomes equal to given number, we store 
-                //the starting and ending index for the subarray.
-                if(currsum==s)
-                {
-                    res.push_back(start + 1);
-                    res.push_back(last + 1);
-                    
-                    //flag is set to true since subarray exists.
-                    flag = true;
-                    break;
-                }
-            }
-        }
+        long long int l=0,r=0;
+        unsigned long long int sum=arr[0];
         
-        if(flag==false)
-            res.push_back(-1);
-            
       
-        return res;    
+        
+        while(r<n and l<=r)
+        {
+          if(sum==s)
+          {
+              return {(l+1),(r+1)};
+          }
+          else if(sum>s)
+          {
+              sum-=arr[l];
+              l++;
+          }
+          else
+          {
+              r++;
+              sum+=arr[r];
+          }
+        }
+         for(long long int i=0;i<n;i++){
+           if(arr[i]==s) return {i+1,i+1};
+       }
+        
+        
+        return {-1};
     }
-
 };
 
 // { Driver Code Starts.
