c6237c4df86b63f770ef07060231b51e179129bf
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-06-22 07:27:42 +0530

    Time: 7 ms (90.68%), Space: 10.1 MB (61.91%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& v, int k) {
-  vector<int>::iterator ip;
-         // ip = std::unique(v.begin(), v.end());
-         // v.resize(std::distance(v.begin(), ip));
-        sort(v.begin(),v.end(),greater<int>());
+  // vector<int>::iterator ip;
+  //        ip = std::unique(v.begin(), v.end());
+  //        v.resize(std::distance(v.begin(), ip));
+        nth_element(v.begin(),v.begin()+(k-1),v.end(),greater<int>());
             return v[k-1];
         
     }
