49c53d29e4b2785272b37b33e7dff5eea7bb2ce1
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-07-31 08:46:22 +0530

    Time: 131 ms (6.05%), Space: 31.1 MB (87.62%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -7,9 +7,13 @@ public:
     int sum=accumulate(v.begin(),v.end(),0);
     for(int i=0;i<v.size();i++){
         left+=v[i];
-       
+        
+        
+        cout<<sum<<"=="<<left<<endl;
         if(left==sum) return i;
-         sum-=v[i];
+        
+        sum-=v[i];
+        
     }
         
         return -1;
