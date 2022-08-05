6f51996ddacd1c3ccf20463fac7098c88be16033
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-08-05 18:52:02 +0530

    Time: 17 ms (5.33%), Space: 7.3 MB (9.25%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    
+    map<int,int> m;
+    
+    int dfs(vector<int> &nums,int target){
+        if(m.find(target)!=m.end()) return m[target];
+        if(target==0) return m[target]=1;
+        if(target<0) return m[target]=0;
+        int ways=0;
+        for(auto &n:nums) ways+=dfs(nums,target-n);
+        return m[target]=ways;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        return dfs(nums,target);
+        
+    }
+};
