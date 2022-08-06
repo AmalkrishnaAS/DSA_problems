030c5ba920ae527257bb18b2ced3bb433ec36d9f
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-08-06 10:08:09 +0530

    Time: 722 ms (9.05%), Space: 10.3 MB (33.94%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
+    
+    //\U0001f622
     int countPrimes(int n) {
-        if(not n) return n;
-       
-       
         
-    vector<bool> prime(n+1,true);
+        if(not n) return 0;
+        vector<bool> prime(n+1,true);
         prime[0]=prime[1]=false;
         int ans=0;
-        
-        for(int i=2;i<n;i++)
-        {
+        for(int i=2;i<n;i++){
             if(prime[i]) ans++;
-            
-          for(int j=2*i;j<=n;j=j+i)
-                prime[j]=false;
+            for(int j=2*i;j<=n;j+=i) prime[j]=false; 
         }
-     
-        
         return ans;
         
-     
     }
 };
