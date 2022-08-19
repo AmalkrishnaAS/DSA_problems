af768585ab6929830209c555766c87c14fb12b69
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-08-19 14:54:03 +0530

    Time: 256 ms (54.78%), Space: 51.2 MB (26.36%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,16 +4,13 @@ public:
         
         set<int> s(nums.begin(),nums.end());
         int ans=0;
-        
         for(auto &n:s){
-            if(s.find(n-1)==s.end()) {
-                int length=0;
+            int length=0;
+            if(s.find(n-1) ==s.end()){
                 while(s.find(n+length)!=s.end()) length++;
                 ans=max(ans,length);
             }
         }
-        
         return ans;
-        
     }
 };
