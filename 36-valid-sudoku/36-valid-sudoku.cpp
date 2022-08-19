0cb2f6e736733791890d08e426688fd7aac0e477
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-08-19 14:45:58 +0530

    Time: 46 ms (30.88%), Space: 21.1 MB (12.37%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        map<int,set<char>> m1,m2;
-        map<pair<int,int>,set<char>> m3;
+        map<int,set<int>> cols;
+        map<int,set<int>> rows;
+        map<pair<int,int>,set<char>> block;
         
         
-        for(int i=0;i<9;i++) {
-            for(int j=0;j<9;j++) {
-                
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 if(board[i][j]=='.') continue;
+                if(rows[i].find(board[i][j]) !=rows[i].end()
+                   or cols[j].find(board[i][j]) !=cols[j].end()
+                   or block[{i/3,j/3}].find(board[i][j]) !=block[{i/3,j/3}].end()
+                  
+                  )   return false;
                 
-                if(m1[i].find(board[i][j]) !=m1[i].end()
-                   or m2[j].find(board[i][j]) !=m2[j].end()
-                   or m3[{i/3,j/3}].find(board[i][j]) !=m3[{i/3,j/3}].end()
-                  )
-                    return false;
                 
-                m1[i].insert(board[i][j]);
-                m2[j].insert(board[i][j]);
-                m3[{i/3,j/3}].insert(board[i][j]);
-                
+                rows[i].insert(board[i][j]);
+                 cols[j].insert(board[i][j]);
+                 block[{i/3,j/3}].insert(board[i][j]);
             }
         }
+        
         return true;
+        
     }
 };
