b2916cd6233703f8029de0be8dae8a3a57927cc4
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-08-19 14:35:15 +0530

    Time: 38 ms (56.30%), Space: 25.2 MB (18.29%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> prefix(n);
-        vector<int> postfix(n);
-        
-        for(int i=0;i<n;i++) prefix[i]=i!=0?nums[i]*prefix[i-1] : nums[0];
-        for(int i=n-1;i>=0;i--) postfix[i]=i!=n-1?nums[i]*postfix[i+1] :nums[n-1];
-        
-        for (auto &n:postfix) cout<<n<<" ";
-            cout<<endl;
-        
-        
+        vector<int> prefix(n),postfix(n);
         vector<int> op(n);
         
         
+        for(int i=0;i<n;i++){
+            prefix[i]=(i==0)?nums[i]:prefix[i-1]*nums[i];
+            
+        }
+        for(int i=n-1;i>=0;i--){
+            postfix[i]=(i==n-1)?nums[n-1]:postfix[i+1]*nums[i];
+        }
+        
         op[0]=postfix[1];
         op[n-1]=prefix[n-2];
-        for(int i=1;i<n-1;i++) op[i]=prefix[i-1] *postfix[i+1];
-        return op;
         
+        for(int i=1;i<n-1;i++) op[i]=prefix[i-1]*postfix[i+1];
+        return op;
     }
 };
