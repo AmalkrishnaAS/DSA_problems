03e200d63aefcfc99ef332f0bc308d38488b5c0c
Amalkrishna A S <77876890+AmalkrishnaAS@users.noreply.github.com>
2022-08-24 15:19:47 +0530

    Time: 16 ms (50.29%), Space: 11.4 MB (53.10%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    vector<int> curr;
     
-    void dfs(vector<int> &nums,vector<int> &curr,int i,int total,int target) {
-        if(i==nums.size() or total>target) {
-            return;
-        }
-        if(total==target) {
+    vector<vector<int>> ans;
+    
+    void dfs(vector<int> &nums,vector<int> &curr,int total,int target,int i){
+        if(total==target ){
             ans.push_back(curr);
             return;
-        }
-        
+        } 
+           if(i==nums.size() or total>target) return;
         curr.push_back(nums[i]);
-        total=total+nums[i];
-        dfs(nums,curr,i,total,target);
+        total+=nums[i];
+        dfs(nums,curr,total,target,i);
         curr.pop_back();
         total-=nums[i];
-        dfs(nums,curr,i+1,total,target);
+        dfs(nums,curr,total,target,i+1);
+        
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
         
-        dfs(candidates,curr,0,0,target);
+        vector<int> curr={};
+        dfs(nums,curr,0,target,0);
         return ans;
-        
-        
     }
 };
